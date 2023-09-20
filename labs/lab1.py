7215a734cdc63f3e9241751a84389bda39b03bf2
proudy9 <144201852+proudy9@users.noreply.github.com>
2023-09-20 11:13:03 +0500

    Сделано 1 задание lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,7 @@
-import math
+from math import *
 
-def task1():
-  pass
+def task1(x, y, z):
+    a = (sinh(x) - sinh(y)) / (cosh(x) + cosh(y))
+    b = (sin(x))**2 + (cos(z))**2 + log(e**(-z) + e**(-9))
+    return a, b
+print(f'Задание 1:\nзначения a и b:{task1(x=int(input("Введите значение x: ")), y=int(input("Введите значение y: ")), z=int(input("Введите значение z: ")))}')
